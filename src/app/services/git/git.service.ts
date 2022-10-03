d0c1164beadf075f085146b5eade4055537a18ca
benie254 <davinci.monalissa@gmail.com>
2022-10-03 20:03:09 +0300

    add get request for repos

--- src/app/services/git/git.service.ts
+++ src/app/services/git/git.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+import { Repo } from 'src/app/classes/repo/repo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitService {
+  baseURL:string= 'https://api.github.com';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  gitRepos(userName: string): Observable<Repo[]>{
+    return this.http.get<Repo[]>(this.baseURL + '/users/' + userName + '/repos');
+  }
 }
