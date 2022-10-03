7a143064a61e180076bb67ddd3bccfbfa6472465
benie254 <davinci.monalissa@gmail.com>
2022-10-03 19:54:48 +0300

    create prerequisite components & import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { RepoComponent } from './components/repo/repo.component';
+import { UserComponent } from './components/user/user.component';
+import { SocialComponent } from './components/social/social.component';
+import { NavComponent } from './nav/nav.component';
+import { TruncatePipe } from './pipes/truncate.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RepoComponent,
+    UserComponent,
+    SocialComponent,
+    NavComponent,
+    TruncatePipe,
   ],
   imports: [
     BrowserModule,
