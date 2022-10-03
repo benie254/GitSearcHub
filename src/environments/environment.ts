869d1fc6a054a8a7b089c0cbda972e2e951f6583
benie254 <davinci.monalissa@gmail.com>
2022-10-03 22:09:28 +0300

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://api.github.com',
+  clientId: '43ec5955c37e77635427',
+  clientSecret: '012b07f6705811dbf464a3246b303baed586e789'
 };
 
 /*
